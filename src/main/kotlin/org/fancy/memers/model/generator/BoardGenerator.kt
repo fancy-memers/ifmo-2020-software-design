cad3e2af6a0f4f002da69cc6897dec158a861f21
Artem Bobrov <artembobrov3059@gmail.com>
2020-05-05 13:58:42 +0200

    renaming

--- src/main/kotlin/org/fancy/memers/model/generator/BoardGenerator.kt
+++ src/main/kotlin/org/fancy/memers/model/generator/BoardGenerator.kt
@@ -4,5 +4,5 @@ import org.fancy.memers.model.Block
 import org.hexworks.zircon.api.data.Position3D
 
 interface BoardGenerator {
-    fun generateMap(isWithPlayer: Boolean = true): Map<Position3D, Block>
+    fun generateMap(withPlayer: Boolean = true): Map<Position3D, Block>
 }
