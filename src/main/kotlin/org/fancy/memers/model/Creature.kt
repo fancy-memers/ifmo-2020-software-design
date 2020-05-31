453893a91328921df9198bc40e0bb2263d387caa
Ivan Posti <ivmposti@gmail.com>
2020-05-31 22:12:56 +0300

    Add stats

---
+++ src/main/kotlin/org/fancy/memers/model/Creature.kt
@@ -0,0 +1,126 @@
+package org.fancy.memers.model
+
+import org.fancy.memers.model.ai.EnemyBehaviour
+import org.hexworks.zircon.api.color.TileColor
+import org.hexworks.zircon.api.data.Position3D
+import org.hexworks.zircon.api.graphics.Symbols
+import java.util.*
+
+sealed class Creature(var position: Position3D): Block() {
+    override fun equals(other: Any?): Boolean = super.equals(other) && position == (other as Creature).position
+    override fun hashCode(): Int = Objects.hashCode(position)
+    override fun toString(): String {
+        return "Entity(level=$level, health=$health)"
+    }
+
+    open val attackMultiplier: Int = 2
+
+    open var level: Int = 1
+    open var experience: Int = 0
+
+    open val givesExperience: Int
+        get() = Math.pow(2.0, level.toDouble()).toInt() + experience
+
+    protected open fun increaseLevel() {
+        level += 1
+        strength += 4
+        agility += 4
+        intelligence += 4
+        health = maxHealth
+    }
+
+    open fun gainExperience(amount: Int): Boolean {
+        val breakpoint = Math.pow(2.0, level.toDouble()).toInt()
+        if (experience + amount >= breakpoint) {
+            experience += amount - breakpoint
+            increaseLevel()
+            return true
+        }
+        experience += amount
+        return false
+    }
+
+    open var strength: Int = DEFAULT_STRENGTH
+    open var agility: Int = DEFAULT_AGILITY
+    open var intelligence: Int = DEAFULT_INTELLIGENCE
+
+    open val maxHealth: Int
+        get() = strength * 4
+
+    open var health: Int = maxHealth
+
+    val attack: Int
+        get() = strength * attackMultiplier
+
+    open val effects = mutableListOf<Effect>()
+
+    open fun updateEffects() {
+        effects.forEach { it.duration -= 1 }
+        effects.removeIf { !it.isActive }
+    }
+
+    protected val initialHealth = health
+    val isDead: Boolean get() = health <= 0
+
+    override val canStepOn: Boolean
+        get() = false
+    override val isVisible: Boolean
+        get() = !isDead
+
+    inline fun <reified EffectType: Effect> hasEffect() = effects.indexOfFirst { it is EffectType } != -1
+
+    companion object {
+        const val DEFAULT_STRENGTH: Int = 5
+        const val DEFAULT_AGILITY: Int = 5
+        const val DEAFULT_INTELLIGENCE: Int = 5
+    }
+}
+
+class Player(position: Position3D) : Creature(position) {
+    override val symbol: Char get() = '@'
+
+    override fun toString(): String = "Player(level=$level, health=$health, effects=$effects)"
+
+//    init {
+//        repeat(4) { increaseLevel() }
+//    }
+
+    /*
+     * Считает текущий цвет
+     * minimumColor – изначальный цвет
+     * maximumColor – цвет при 0 значении health
+     */
+    override val foregroundColor: TileColor
+        get() = gradientColor(
+            TileColor.fromString("#00ff00"),
+            TileColor.fromString("#ffff00"),
+            -(initialHealth - health) / initialHealth.toDouble()
+        )
+    override val backgroundColor: TileColor get() = TileColor.fromString("#1e2320")
+    override val displayName: String
+        get() = "Player"
+}
+
+class Enemy(private val name: String, val behaviour: EnemyBehaviour, position: Position3D) : Creature(position) {
+    override val symbol: Char get() = Symbols.EPSILON
+
+    override fun toString(): String =
+        "Enemy(level=$level, health=$health, behaviour=$behaviour, effects=$effects)"
+
+    override val attackMultiplier: Int = 1
+
+    /*
+     * Считает текущий цвет
+     * minimumColor – изначальный цвет
+     * maximumColor – цвет при 0 значении health
+     */
+    override val foregroundColor: TileColor
+        get() = gradientColor(
+            minimumColor = TileColor.fromString("#ff0000"),
+            maximumColor = TileColor.fromString("#ffff00"),
+            ratio = (initialHealth - health) / initialHealth.toDouble()
+        )
+    override val backgroundColor: TileColor get() = TileColor.fromString("#1e2320")
+    override val displayName: String
+        get() = name
+}
--- src/main/kotlin/org/fancy/memers/ui/main/InfoPanelFragment.kt
+++ src/main/kotlin/org/fancy/memers/ui/main/InfoPanelFragment.kt
@@ -1,17 +1,56 @@
 package org.fancy.memers.ui.main
 
+import org.fancy.memers.ui.main.board.World
+import org.hexworks.cobalt.events.api.Event
+import org.hexworks.cobalt.events.api.KeepSubscription
+import org.hexworks.cobalt.events.api.subscribeTo
 import org.hexworks.zircon.api.Components
-import org.hexworks.zircon.api.component.Component
 import org.hexworks.zircon.api.component.ComponentAlignment
 import org.hexworks.zircon.api.component.Fragment
-import org.hexworks.zircon.api.extensions.box
-import org.hexworks.zircon.api.graphics.BoxType
 import org.hexworks.zircon.api.grid.TileGrid
+import org.hexworks.zircon.internal.Zircon
+
+class WorldUpdate(val world: World, override val emitter: Any): Event {
+    companion object {
+        fun publish(world: World) {
+            Zircon.eventBus.publish(WorldUpdate(world, this))
+        }
+    }
+}
 
 class InfoPanelFragment(parent: TileGrid) : Fragment {
-    override val root: Component = Components.panel()
+    override val root = Components.vbox()
         .withSize(MainScreenConfig.infoPanelSize(parent))
         .withAlignmentWithin(parent, ComponentAlignment.RIGHT_CENTER)
-        .withDecorations(box(BoxType.SINGLE))
         .build()
+
+
+    private val levelLine = Components.label()
+        .withSize(root.width, 1).build()
+    private val healthLine = Components.label()
+        .withSize(root.width, 1).build()
+    private val strengthLine = Components.label()
+        .withSize(root.width, 1).build()
+    private val agilityLine = Components.label()
+        .withSize(root.width, 1).build()
+    private val intelligenceLine = Components.label()
+        .withSize(root.width, 1).build()
+
+    init {
+        root.addComponent(levelLine)
+        root.addComponent(healthLine)
+        root.addComponent(strengthLine)
+        root.addComponent(agilityLine)
+        root.addComponent(intelligenceLine)
+        Zircon.eventBus.subscribeTo<WorldUpdate> {
+            it.world.player.run {
+                levelLine.text = "level: $level exp: $experience"
+                healthLine.text = "health: $health/$maxHealth"
+                strengthLine.text = "str: $strength"
+                agilityLine.text = "agi: $agility"
+                intelligenceLine.text = "str: $intelligence"
+            }
+            KeepSubscription
+        }
+    }
 }
